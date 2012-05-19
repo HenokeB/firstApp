39b747dd24ed161668b6c4fbedc9c12cd3efd1f8
Henoke Benebaru Hailegiorgis <hb@hoas-fe3add00-29.dhcp.inet.fi>
2012-05-19 03:05:49 +0300

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '01d809f788584219bea5c43a445f6b1a0120d8bc8ff6c1a9afe318bd8270c899085adcfdc8310d404e2dbed03e603ef88442e36f6fb2fc22aae31e8f3e2bd4e1'
